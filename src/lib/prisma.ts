13b2be39f2530a76c86bf9ec5834ffab692c804d
shibili <shibilipadalath@gmail.com>
2026-04-18 16:03:50 +0530

    Add missing type definitions

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,10 @@
 import "dotenv/config";
 import { PrismaNeon } from '@prisma/adapter-neon';
 import { PrismaClient } from "@prisma/client";
+import { neonConfig } from '@neondatabase/serverless';
+import ws from 'ws';
+
+neonConfig.webSocketConstructor = ws;
 
 const connectionString = `${process.env.DATABASE_URL}`;
 const adapter = new PrismaNeon({ connectionString });
